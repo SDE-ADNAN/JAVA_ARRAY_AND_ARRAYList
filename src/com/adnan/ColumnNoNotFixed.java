d0683d154e8e4909a5ca1a5cf88df52d21c96f93
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-03-21 23:53:14 +0530

    created my own 2d array

---
+++ src/com/adnan/ColumnNoNotFixed.java
@@ -0,0 +1,24 @@
+package com.adnan;
+
+import java.util.Arrays;
+
+public class ColumnNoNotFixed {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {1,2,3,4},
+                {5,6},
+                {7,8,9}
+        };
+
+        for (int rows = 0; rows < arr.length; rows++) {
+
+            for (int col = 0; col <arr[rows].length; col++) {
+                System.out.print(arr[rows][col]+" ");
+
+
+            }
+            System.out.println(Arrays.toString(arr[rows]));
+
+        }
+    }
+}
