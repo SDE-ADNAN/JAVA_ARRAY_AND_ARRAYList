7420fc6e4587f857727abbf7fec5561b79a95f47
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-03-22 08:00:30 +0530

    made array indices swap function

--- src/com/adnan/MultiDimensionalArraylist.java
+++ src/com/adnan/MultiDimensionalArraylist.java
@@ -8,10 +8,12 @@ public class MultiDimensionalArraylist {
         Scanner in = new Scanner(System.in);
         ArrayList<ArrayList<Integer>> list = new ArrayList<>();
 
+        //initialisation
         for (int i = 0; i < 3; i++) {
             list.add(new ArrayList<>());
         }
 
+        // adding elements
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 list.get(i).add(in.nextInt());
